a05331062e10afc460a33a3b94061f73175896c4
Qasim Rahman <RahmanQY@Cardiff.ac.uk>
2026-05-05 22:00:11 +0100

    Added more items to final submission

--- shop_app/storeItems.py
+++ shop_app/storeItems.py
@@ -29,6 +29,34 @@ items = [
         "description": "A pair of durable football boots designed for firm ground play.",
         "image": "football-boots.jpeg",
         "environmentalImpactRating": 4
+    },
+    {
+        "name": "Panic! At the Disco - Vices & Virtues Vinyl",
+        "price": 100,
+        "description": "A vinyl of the hit Album 'Vices & Virtues' by Panic! At the Disco.",
+        "image": "VicesandVirtuesVinyl.jpeg",
+        "environmentalImpactRating": 1
+    },
+    {
+        "name": "Graphic Sonic the Hedgehog Poster",
+        "price": 30,
+        "description": "A minimalistic poster of Sonic the Hedgehog with modern/flat illustration with a bit of vector art mixed.",
+        "image": "SonicPoster.jpeg",
+        "environmentalImpactRating": 3
+    },
+        {
+        "name": "Minecraft Torch LED Key Ring",
+        "price": 5,
+        "description": "Minecraft torch key ring with a LED light you can turn on and off by the press of a button.",
+        "image": "MinecraftTorchKeyRing.jpeg",
+        "environmentalImpactRating": 3
+    },
+    {
+        "name": "Pokémon Plushie Set",
+        "price": 60,
+        "description": "Starter Pokémon Plushie set (Inc. Pikachu)",
+        "image": "pokemonplushieset.jpeg",
+        "environmentalImpactRating": 2
     }
 ]
 
